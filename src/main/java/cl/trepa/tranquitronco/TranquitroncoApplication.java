1fd9d45dfad9a49a07f18098763dc6e9bf27a392
AMARU FERNANDEZ <afernandez@altiuz.cl>
2019-01-29 15:46:55 -0300

    inicializa proyecto

---
+++ src/main/java/cl/trepa/tranquitronco/TranquitroncoApplication.java
@@ -0,0 +1,14 @@
+package cl.trepa.tranquitronco;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TranquitroncoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TranquitroncoApplication.class, args);
+	}
+
+}
+
---
+++ src/test/java/cl/trepa/tranquitronco/TranquitroncoApplicationTests.java
@@ -0,0 +1,17 @@
+package cl.trepa.tranquitronco;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class TranquitroncoApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
+
